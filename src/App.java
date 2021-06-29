3c97e4bf8eb54ff6c8a163a113af74b674958ce5
AnaPatriciaPerezRios <ana_patri2003@yahoo.com.mx>
2021-06-29 08:52:47 -0600

    Presentación unidad

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+System.out.println(entero16bits);
+
+int [] lista = {2,5,6,3,6};
+System.out.println(Lista [0]);
